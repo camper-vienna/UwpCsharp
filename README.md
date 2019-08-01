4fcb5a72f51a15b8d658cf30d7771f93cd6ded01
Your Name <you@example.com>
2019-08-01 14:46:29 +0200

    UWP throw from C#

---
+++ README.md
@@ -0,0 +1,6 @@
+UnhandledException is not captured at the time.
+
+Handled is captured in Debug and Release builds, fired up from within Visual Studio and also _Deployed_ to Windows 10.
+With PDB available the stack trace is correctly sent from the client:
+
+![Sample event in Sentry](UWP-CSharp-Windows10.png)
---
+++ UwpCsharp/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Sentry;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpCsharp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void ThrowUnhandledExceptionClick(object sender, RoutedEventArgs e) => TheOffender();
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ThrowUnhandledExceptionClick(this, e);
+            }
+            catch (Exception ex)
+            {
+                SentrySdk.WithScope(s =>
+                {
+                    s.AddBreadcrumb(
+                        nameof(Button_Click),
+                        category: "click",
+                        type: "handled",
+                        dataPair: (nameof(e.OriginalSource), e.OriginalSource.ToString()));
+
+                    SentrySdk.CaptureException(ex);
+                });
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void TheOffender() => throw new Exception("No one will catch me!");
+    }
+}
